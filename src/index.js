ac1b53fbeccd83fb3bda5df804b23cdf5758c1fe
Greg <caldwell.gregoryj@gmail.com>
2021-04-14 10:29:48 -0400

    Started the assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 let addToy = false;
+let toyBox = document.querySelector("#toy-collection")
+let form = document.querySelector(".add-toy-form")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  
+  //let toyBox = document.querySelector("#toy-collection")
+  //console.log(document.querySelector("#toy-collection"))
+
+  fetch("http://localhost:3000/toys/")
+    .then(res => res.json())
+    .then((toysArray) => {
+      toysArray.forEach( function(toyObj) {
+        appendToys(toyObj)
+      })
+    })
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function appendToys(toyObj) {
+  let newToyCard = document.createElement("div")
+  newToyCard.className = "card"
+  toyBox.append(newToyCard)
+  
+
+
+}
